08752896b25b72f5d3081480cdeb8f81e2c6b541
Ralf Meermeier <ralf.meermeier@gmail.com>
2018-01-25 20:20:19 -0500

    Run on Pi

--- linux/BluetootKeyboard.py
+++ linux/BluetootKeyboard.py
@@ -8,16 +8,22 @@
 import uinput
 from bluetooth import *
 
-device = uinput.Device([
-    uinput.KEY_X,
-    uinput.KEY_Y,
-    uinput.KEY_A,
-    uinput.KEY_B,
-    uinput.KEY_UP,
-    uinput.KEY_LEFT,
-    uinput.KEY_RIGHT,
-    uinput.KEY_DOWN
-    ])
+uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+
+keyMap = {
+        "KEYCODE_DPAD_UP" : uinput.KEY_DPAD_UP,
+        "KEYCODE_DPAD_LEFT" : uinput.KEY_DPAD_LEFT,
+        "KEYCODE_DPAD_RIGHT" : uinput.KEY_DPAD_RIGHT,
+        "KEYCODE_DPAD_DOWN" : uinput.KEY_DPAD_DOWN,
+        "KEYCODE_BUTTON_START" : uinput.KEY_BTN_START,
+        "KEYCODE_BUTTON_SELECT" : uinput.KEY_BTN_SELECT,
+        "KEYCODE_BUTTON_X" : uinput.BTN_X,
+        "KEYCODE_BUTTON_Y" : uinput.BTN_Y,
+        "KEYCODE_BUTTON_A" : uinput.BTN_A,
+        "KEYCODE_BUTTON_B" : uinput.BTN_B
+        }
+
+device = uinput.Device(list(keyMap.values()))
 
 server_sock=BluetoothSocket( RFCOMM )
 server_sock.bind(("",PORT_ANY))
@@ -25,46 +31,40 @@ server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+advertise_service( server_sock, "BluetoothKeyboard",
+        service_id = uuid,
+        service_classes = [ uuid, SERIAL_PORT_CLASS ],
+        profiles = [ SERIAL_PORT_PROFILE ], 
+        #                   protocols = [ OBEX_UUID ] 
+        )
 
-advertise_service( server_sock, "SampleServer",
-                   service_id = uuid,
-                   service_classes = [ uuid, SERIAL_PORT_CLASS ],
-                   profiles = [ SERIAL_PORT_PROFILE ], 
-#                   protocols = [ OBEX_UUID ] 
-                    )
-
-print "Waiting for connection on RFCOMM channel %d" % port
-
-client_sock, client_info = server_sock.accept()
-print "Accepted connection from ", client_info
-
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        if data == "KEY_X":
-            device.emit_click(uinput.KEY_X)
-        elif data == "KEY_Y":
-            device.emit_click(uinput.KEY_Y)
-        elif data == "KEY_A":
-            device.emit_click(uinput.KEY_A)
-        elif data == "KEY_B":
-            device.emit_click(uinput.KEY_B)
-        elif data == "KEY_UP":
-            device.emit_click(uinput.KEY_UP)
-        elif data == "KEY_LEFT":
-            device.emit_click(uinput.KEY_LEFT)
-        elif data == "KEY_RIGHT":
-            device.emit_click(uinput.KEY_RIGHT)
-        elif data == "KEY_DOWN":
-            device.emit_click(uinput.KEY_DOWN)
-except IOError:
-    pass
-
-print "disconnected"
-
-client_sock.close()
+while True:
+    client_sock, client_info = server_sock.accept()
+    print "Accepted connection from ", client_info
+    
+    try:
+        while True:
+            data = client_sock.recv(1024)
+            if len(data) == 0: break
+            if data == "KEY_X":
+                device.emit_click(uinput.KEY_X)
+            elif data == "KEY_Y":
+                device.emit_click(uinput.KEY_Y)
+            elif data == "KEY_A":
+                device.emit_click(uinput.KEY_A)
+            elif data == "KEY_B":
+                device.emit_click(uinput.KEY_B)
+            elif data == "KEY_UP":
+                device.emit_click(uinput.KEY_UP)
+            elif data == "KEY_LEFT":
+                device.emit_click(uinput.KEY_LEFT)
+            elif data == "KEY_RIGHT":
+                device.emit_click(uinput.KEY_RIGHT)
+            elif data == "KEY_DOWN":
+                device.emit_click(uinput.KEY_DOWN)
+    except IOError:
+        client_sock.close()
+        continue
+    
 server_sock.close()
-print "all done"
 
