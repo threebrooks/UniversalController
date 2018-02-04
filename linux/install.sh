26a138d363c53daba5734bf833237037ceca120c
Ralf Meermeier <ralf.meermeier@gmail.com>
2018-02-04 19:31:47 +0000

    fixes

--- linux/install.sh
+++ linux/install.sh
@@ -6,20 +6,33 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
-echo Patching bluez service to be backward compatible
-perl -pi.bak -e 's/(ExecStart=\/usr\/lib\/bluetooth\/bluetoothd\s*)/\1\ -C/' /etc/systemd/system/dbus-org.bluez.service 
-rm /etc/systemd/system/dbus-org.bluez.service.bak 
-sdptool add SP
-systemctl restart bluetooth
-
-echo Installing Python dependencies
-apt-get -y install python-pip python-bluetooth 
+echo Installing dependencies
+apt-get -y install bluetooth bluez bluez-tools bluez-firmware python-pip python-bluetooth 
 yes | pip install python-uinput
 
+echo Enabling bluetooth service
+systemctl enable bluetooth.service  || true
+echo Stopping bluetooth service
+systemctl stop bluetooth.service
+
+echo Patching bluez service to be backward compatible
+perl -pi.bak -e 's/(ExecStart=\/usr\/lib\/bluetooth\/bluetoothd\ *)$/\1\ -C/' /etc/systemd/system/dbus-org.bluez.service 
+perl -pi.bak -e 's/(ExecStart=\/usr\/lib\/bluetooth\/bluetoothd\ *)$/\1\ -C/' /etc/systemd/system/bluetooth.target.wants/bluetooth.service 
+rm /etc/systemd/system/dbus-org.bluez.service.bak 
+systemctl restart bluetooth
+echo Adding SP
+sdptool add SP 
+echo Restarting bluetooth service
+
+echo Adding modules to /etc/modules
 if grep -q "uinput" /etc/modules; then
    echo
 else
    echo "uinput" >> /etc/modules
+fi
+if grep -q "evdev" /etc/modules; then
+   echo
+else
    echo "evdev" >> /etc/modules
 fi
 
