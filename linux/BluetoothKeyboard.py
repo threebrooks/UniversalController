1134047beed78c124cb98c46f5e24cef2c0ba434
Ralf Meermeier <ralf.meermeier@gmail.com>
2018-03-08 01:45:19 +0000

    Better msg handling

--- linux/BluetoothKeyboard.py
+++ linux/BluetoothKeyboard.py
@@ -58,47 +58,61 @@ while True:
         client_sock.send(APP_VERSION)
     
         while True:
-            data = client_sock.recv(1024)
-            if len(data) == 0: continue
-            for msgString in str(data).split("@@@"):
-              if (msgString == ""): continue
-              #print msgString
-              js = json.loads(msgString)
-              type = js['type']
-              if (type == "KEY"):
-                if (js['pressed'] == "true"):
-                  press = 1
-                else:
-                  press = 0
-                for keyStringEl in js['keylist'].split("|"):
-                  if (keyStringEl == "HEARTBEAT"): continue
-                  key = getattr(uinput, keyStringEl);
-                  #print "Emitting "+keyStringEl
-                  device.emit(key, press)
-              elif (type == "MOUSE"):
-                if (js['absrel'] == "REL"):
-                  xAccum += js['dx'] 
-                  yAccum += js['dy'] 
-                  xError = xAccum-lastXInt
-                  yError = yAccum-lastYInt
-                  if (abs(xError) > 0.5 or abs(yError) > 0.5):
-                    newX = int(round(xAccum))
-                    newY = int(round(yAccum))
-                    deltaX = newX-lastXInt
-                    deltaY = newY-lastYInt
-                    if (deltaX != 0 or deltaY != 0):
-                      #print "Emitting mouse move "+str(deltaX)+","+str(deltaY)
-                      device.emit(uinput.REL_X, newX-lastXInt, syn=False)
-                      device.emit(uinput.REL_Y, newY-lastYInt)
-                      lastXInt = newX
-                      lastYInt = newY
-                elif (js['absrel'] == "ABS"):
-                  device.emit(uinput.ABS_X, int(js['x']), syn=False)
-                  device.emit(uinput.ABS_Y, int(js['y']))
+            msgLengthString = ""
+            while True:
+              c = client_sock.recv(1)
+              if len(c) == 0: continue
+              if (c[0] == ":"): break
+              msgLengthString += c[0]
+            #print "Msg: '"+msgLengthString+"'"
+            msgLength = int(msgLengthString)
+            pulledMsgLength = 0
+            data = []
+            while True:
+              c = client_sock.recv(msgLength-pulledMsgLength)
+              if len(c) == 0: continue
+              data += c
+              pulledMsgLength = pulledMsgLength + len(c)
+              if (pulledMsgLength == msgLength): break
+            msgString = "".join(data)
+            #print msgString
+            js = json.loads(msgString)
+            type = js['type']
+            if (type == "KEY"):
+              if (js['pressed'] == "true"):
+                press = 1
+              else:
+                press = 0
+              for keyStringEl in js['keylist'].split("|"):
+                if (keyStringEl == "HEARTBEAT"): continue
+                key = getattr(uinput, keyStringEl);
+                #print "Emitting "+keyStringEl
+                device.emit(key, press)
+            elif (type == "MOUSE"):
+              if (js['absrel'] == "REL"):
+                xAccum += js['dx'] 
+                yAccum += js['dy'] 
+                xError = xAccum-lastXInt
+                yError = yAccum-lastYInt
+                if (abs(xError) > 0.5 or abs(yError) > 0.5):
+                  newX = int(round(xAccum))
+                  newY = int(round(yAccum))
+                  deltaX = newX-lastXInt
+                  deltaY = newY-lastYInt
+                  if (deltaX != 0 or deltaY != 0):
+                    #print "Emitting mouse move "+str(deltaX)+","+str(deltaY)
+                    device.emit(uinput.REL_X, newX-lastXInt, syn=False)
+                    device.emit(uinput.REL_Y, newY-lastYInt)
+                    lastXInt = newX
+                    lastYInt = newY
+              elif (js['absrel'] == "ABS"):
+                device.emit(uinput.ABS_X, int(js['x']), syn=False)
+                device.emit(uinput.ABS_Y, int(js['y']))
 
     except:
-        print("Lost connection\n"+str(sys.exc_info()[0]))
+        print("Lost connection\n"+str(sys.exc_info()[0])+"\n"+str(sys.exc_info()[2].tb_lineno))
         if (client_sock != None): client_sock.close()
+        #sys.exit(-1)
         continue
     
 server_sock.close()
